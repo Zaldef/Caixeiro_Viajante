60e3dce2552a383faed36777fa79810f6a808024
Guilherme Roelli <78030935+Zaldef@users.noreply.github.com>
2024-11-13 12:08:09 -0300

    Teste 01

--- TSP_GENETICO.py
+++ TSP_GENETICO.py
@@ -42,6 +42,17 @@ def plotar_rota(pontos, rota, titulo):
     plt.title(f"{titulo}: Melhor distância = {1/ aptidao(rota, pontos)}")
     plt.savefig(titulo, dpi=300)
 
+def plotar_aptidao(historico_aptidao, gen, titulo):
+    """
+    Plota a evolução da aptidão ao longo das gerações.
+    """
+    plt.clf()
+    plt.plot(historico_aptidao)
+    plt.title(f"{titulo}: Melhor geração = {gen}")
+    plt.xlabel("Geração")
+    plt.ylabel("Melhor Distância")
+    plt.savefig(titulo, dpi=300)
+
 def distancia(ponto1, ponto2):
     """
     Calcula a distância euclidiana entre dois pontos.
@@ -62,10 +73,10 @@ def aptidao(rota, pontos):
 
 def selecao(populacao, aptidoes):
     """
-    Seleciona um indivíduo da população com base em um torneio de 3 indivíduos.
+    Seleciona um indivíduo da população com base em um torneio de 5 indivíduos.
     """
 
-    torneio = random.sample(list(zip(populacao, aptidoes)), 3)
+    torneio = random.sample(list(zip(populacao, aptidoes)), 5)
     torneio.sort(key=lambda x: x[1], reverse=True)
 
     return torneio[0][0]
@@ -123,9 +134,11 @@ def elitismo(populacao, aptidoes, taxa_mutacao, pontos, elite=True):
     nova_populacao = []
 
     if elite:
-        melhor_indice = np.argmax(aptidoes)
-        melhor_individuo = populacao[melhor_indice]
-        nova_populacao.append(melhor_individuo)
+        num_elite = max(1, int(0.05 * len(populacao)))
+        melhores_indices_5 = np.argsort(aptidoes)[-num_elite:]
+        
+        for indice in melhores_indices_5:
+            nova_populacao.append(populacao[indice])
 
     for _ in range(len(populacao) - len(nova_populacao)):
         pai1 = selecao(populacao, aptidoes)
@@ -136,7 +149,7 @@ def elitismo(populacao, aptidoes, taxa_mutacao, pontos, elite=True):
 
     return nova_populacao
 
-def algoritmo_genetico(pontos, teste, tamanho_populacao=200, num_geracoes=200, taxa_mutacao=0.16, elite=True):
+def algoritmo_genetico(pontos, teste, tamanho_populacao, num_geracoes, taxa_mutacao, elite):
     """
     Algoritmo Genético para resolver o Problema do Caixeiro Viajante.
     """
@@ -144,58 +157,99 @@ def algoritmo_genetico(pontos, teste, tamanho_populacao=200, num_geracoes=200, t
     populacao = [criar_rota_aleatoria(pontos) for _ in range(tamanho_populacao)]
     historico_aptidao = []
 
-    melhor_rota = None
+    melhor_rota_ = None
+    melhor_geracao_ = None
+    melhor_aptidao = None
 
     for geracao in range(num_geracoes):
         aptidoes = [aptidao(rota, pontos) for rota in populacao]
+        if melhor_aptidao is None or max(aptidoes) > melhor_aptidao:
+            melhor_geracao_ = geracao + 1
         melhor_aptidao = max(aptidoes)
-        melhor_rota = populacao[aptidoes.index(melhor_aptidao)]
+        melhor_rota_ = populacao[aptidoes.index(melhor_aptidao)]
         historico_aptidao.append(1 / melhor_aptidao)
 
-        print(f"Geração {geracao + 1}: Melhor distância: {1 / melhor_aptidao}")
+        # print(f"Geração {geracao + 1} /  Melhor distância: {1 / melhor_aptidao} / melhor geração: {melhor_geracao_}")
 
         populacao = elitismo(populacao, aptidoes, taxa_mutacao, pontos, elite)
 
-         # Atualizar gráfico do rota a cada 10 gerações
-        if (geracao + 1) % 10 == 0:
-            # Desenha a melhor rota da geração
-            rota_completo = melhor_rota + [melhor_rota[0]]  # Volta ao ponto inicial
-            x = [pontos[i][0] for i in rota_completo]
-            y = [pontos[i][1] for i in rota_completo]
+        # if (geracao + 1) % 10 == 0 or geracao == 0:
+        #     rota_completo = melhor_rota_ + [melhor_rota_[0]] 
+        #     x = [pontos[i][0] for i in rota_completo]
+        #     y = [pontos[i][1] for i in rota_completo]
+        #     plt.clf()
+        #     plt.scatter([p[0] for p in pontos], [p[1] for p in pontos], color='red')
+        #     plt.plot(x, y, 'b-', marker='o', markersize=5)
+        #     plt.title(f"Geração {geracao + 1}: Melhor distância = {1 / melhor_aptidao}")
+        #     plt.xlim(-16, 16) 
+        #     plt.ylim(-16, 16)
+        #     plt.savefig(f'{teste}_gen_{geracao + 1}.png', dpi=300)
 
-            plt.clf()  # Limpa o gráfico anterior
-            plt.scatter([p[0] for p in pontos], [p[1] for p in pontos], color='red')  # Plota os pontos
-            plt.plot(x, y, 'b-', marker='o', markersize=5)  # Plota a rota atual
-            plt.title(f"Geração {geracao + 1}: Melhor distância = {1 / melhor_aptidao}")
-            plt.xlim(-15, 15)  # Ajuste os limites conforme seus dados
-            plt.ylim(-15, 15)
-
-            # Salva a imagem da rota
-            plt.savefig(f'{teste}_gen_{geracao + 1}.png', dpi=300)
-
-
-    return melhor_rota, historico_aptidao
+    return melhor_rota_, historico_aptidao, melhor_geracao_
 
 
 
 # -------------- MAIN --------------
 
-# Teste Pontos Uniformes - 15 pontos
-pontos_uniformes = criar_pontos_uniformes(150)
-plotar_rota(pontos_uniformes, criar_rota_aleatoria(pontos_uniformes), "Pontos Uniformes Aleatórios")
-melhor_rota, historico = algoritmo_genetico(pontos_uniformes,"Pontos Uniformes ")
-plotar_rota(pontos_uniformes, melhor_rota, "Rota Uniforme Otimizida")
+matriz_resultados= np.zeros((10,3,4,4,4,2))
+testes = [1,2,3,4,5,6,7,8,9,10]
+pontos_ = [criar_pontos_circulares(20), criar_pontos_uniformes(20), criar_pontos_circulares(200)]
+populacoes = [50, 100, 150, 200]
+geracoes = [50, 100, 150, 200]
+taxas_mutacao = [0.025, 0.05, 0.075, 0.1]
+elite_ = [True, False]
 
-## Teste Pontos Circulares - 15 pontos
-pontos_circulares = criar_pontos_circulares(150)
-plotar_rota(pontos_circulares, criar_rota_aleatoria(pontos_circulares), "Pontos Circulares Aleatórios")
-melhor_rota, historico = algoritmo_genetico(pontos_circulares,"Pontos Circulares")
-plotar_rota(pontos_circulares, melhor_rota, "Rota Circular otimizada")
+for i in range(10):
+    np.save("matriz_resultados.npy", matriz_resultados)
+    for j in range(3):
+        for k in range(4):
+            for l in range(4):
+                for m in range(4):
+                    for n in range(2):
+                        melhor_rota, historico, melhor_gen = algoritmo_genetico(pontos_[j],
+                                                                            teste = f"Teste {i+1}. {j+1}. {k+1}. {l+1}. {m+1}. {n+1}",
+                                                                            tamanho_populacao=populacoes[k],
+                                                                            num_geracoes=geracoes[l],
+                                                                            taxa_mutacao=taxas_mutacao[m],
+                                                                            elite=elite_[n])
+                        plotar_aptidao(historico, melhor_gen, f"Teste {i+1}. {j+1}. {k+1}. {l+1}. {m+1}. {n+1}")
+                        matriz_resultados[i,j,k,l,m,n] = 1 / aptidao(melhor_rota, pontos_[j])
 
-# Plotar histórico de aptidão
-plt.clf()
-plt.plot(historico)
-plt.title("Evolução da aptidão ao Longo das Gerações")
-plt.xlabel("Geração")
-plt.ylabel("Melhor Distância")
-plt.savefig('historico_aptidao', dpi=300)
+
+
+
+# # Teste Pontos Circulares - 20 pontos
+# pontos_circulares = criar_pontos_circulares(20)
+# plotar_rota(pontos_circulares, criar_rota_aleatoria(pontos_circulares), "Pontos Circulares Aleatórios")
+# melhor_rota, historico, melhor_gen = algoritmo_genetico(pontos = pontos_circulares,
+#                                             teste = "Pontos Circulares Simples",
+#                                             tamanho_populacao=100,
+#                                             num_geracoes=30,
+#                                             taxa_mutacao=0.02,
+#                                             elite=True)
+# plotar_rota(pontos_circulares, melhor_rota, "Rota Circular Simples Otimizada")
+# plotar_aptidao(historico, melhor_gen, "Evolução da Aptidão Pontos Circulares Simples")
+
+# # Teste Pontos Uniformes - 20 pontos
+# pontos_uniformes = criar_pontos_uniformes(20)
+# plotar_rota(pontos_uniformes, criar_rota_aleatoria(pontos_uniformes), "Pontos Uniformes Aleatórios")
+# melhor_rota, historico, melhor_gen = algoritmo_genetico(pontos = pontos_uniformes,
+#                                             teste = "Pontos Uniformes Simples",
+#                                             tamanho_populacao=100,
+#                                             num_geracoes=200,
+#                                             taxa_mutacao=0.02,
+#                                             elite=True)
+# plotar_rota(pontos_uniformes, melhor_rota, "Rota Uniforme Simples Otimizada")
+# plotar_aptidao(historico, melhor_gen, "Evolução da Aptidão Pontos Uniformes Simples")
+
+# # Teste Pontos Circulares - 200 pontos
+# pontos_circulares = criar_pontos_circulares(200)
+# plotar_rota(pontos_circulares, criar_rota_aleatoria(pontos_circulares), "Pontos Circulares Aleatórios")
+# melhor_rota, historico, melhor_gen = algoritmo_genetico(pontos = pontos_circulares,
+#                                             teste = "Pontos Circulares Complexos",
+#                                             tamanho_populacao=200,
+#                                             num_geracoes=200,
+#                                             taxa_mutacao=0.1,
+#                                             elite=False)
+# plotar_rota(pontos_circulares, melhor_rota, "Rota Circular Complexa Otimizada")
+# plotar_aptidao(historico, melhor_gen, "Evolução da Aptidão Pontos Circulares Complexos")
